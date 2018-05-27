e5408f57c88b07232f11066b55ba5610a1534546
dwil2444 <daneandrew16@gmail.com>
2018-05-27 01:07:56 -0400

    added relevant NuGet packages and VisionDialog class

--- VizionBot/Dialogs/RootDialog.cs
+++ VizionBot/Dialogs/RootDialog.cs
@@ -10,22 +10,20 @@ namespace VizionBot.Dialogs
     {
         public Task StartAsync(IDialogContext context)
         {
+            context.PostAsync("Give Me An Image Url");
             context.Wait(MessageReceivedAsync);
-
             return Task.CompletedTask;
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
+            context.Call(new VisionDialog(activity.Text), VisDialogResume);
+        }
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+        private async Task VisDialogResume(IDialogContext context, IAwaitable<object> result)
+        {
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            context.Wait(MessageReceivedAsync);
         }
     }
 }
